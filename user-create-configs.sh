5f119a20583060d7a60a17fd1e11e460d0693f30
Alexander Zhukov <zuav@yandex.ru>
2011-11-01 17:24:00 +0400

    first version.

---
+++ user-create-configs.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+cd
+
+base_dir=~/src/configs
+backup_dir=~/configs.orig/`date +%s`
+mkdir -p $backup_dir > /dev/null
+
+#
+# create common config files
+#
+
+common_list=".ratpoisonrc .xbindkeysrc .xsession .emacs"
+
+for i in $common_list
+do
+    mv $i $backup_dir/
+    ln -sf $base_dir/common/$i $i
+done
+
+
+#
+# create host specific config files
+#
+
+hostname=`hostname -s`
+host_list=".gnus.el"
+
+if [ ! -d $base_dir/$hostname ]
+then
+    echo "No config dir for host: $hostname"
+else
+    for i in $host_list
+    do
+        mv $i $backup_dir/
+        ln -sf $base_dir/$hostname/$i $i
+    done
+fi
+
+
+cd -
