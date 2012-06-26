4ec98a9363a995af171dd56b6e3de98e0c26a160
Alexander Zhukov <zuav@yandex.ru>
2012-06-26 14:14:55 +0400

    Some new files added, some old ones modified.

--- ithilien/.gnus.el
+++ ithilien/.gnus.el
@@ -1,6 +1,6 @@
 ;;; .gnus.el --- Gnus
 
-;; Copyright (C) 2006, 2007, 2009, 2010, 2011  Alexander Zhukov
+;; Copyright (C) 2006, 2007, 2009, 2010, 2011, 2012  Alexander Zhukov
 
 ;; Author: Alexander Zhukov
 ;; Keywords: extensions
@@ -15,19 +15,23 @@
 
 (gnus-demon-add-handler 'gnus-demon-scan-news 30 7)
 
-(setq user-mail-address             "zhukov@altell.ru"
+(setq user-mail-address             "zuav@yandex.ru"
       send-mail-function            'smtpmail-send-it
       message-send-mail-function    'smtpmail-send-it
-      smtpmail-smtp-server          "mail.altell.local"
-      smtpmail-auth-credentials     (expand-file-name "~/.authinfo")
+      smtpmail-smtp-server          "smtp.yandex.ru"
+      smtpmail-auth-credentials     '(("smtp.yandex.ru" 25 "zuav" nil))
+      smtpmail-starttls-credentials '(("smtp.yandex.ru" 465 nil nil))
       smtpmail-debug-info           t
       smtpmail-debug-verb           t)
 
 (setq gnus-select-method '(nntp "news.eternal-september.org"))
 
 (setq gnus-secondary-select-methods
-      '((nnimap "altell"
-                (nnimap-address "mail"))))
+      '((nnimap "yandex"
+                (nnimap-address       "imap.yandex.ru")
+		(nnimap-server-port   993)
+                (nnimap-stream        ssl)
+                (nnimap-authinfo-file "/home/zuav/.authinfo"))))
 
 (setq gnus-message-archive-group "sent-mail")
 (setq gnus-message-archive-method
@@ -40,24 +44,24 @@
       nnimap-split-inbox     "INBOX"
       imap-log               nil)
 
-(setq nnimap-split-rule
-      '(("INBOX.cleanvm-trustedarm"   "^Subject: trustedarm.*build on .+")
-        ("INBOX.cleanvm-apkr10"       "^Subject: apkr10.*build on .+")
-        ("INBOX.cleanvm-bereg"        "^Subject: bereg.*build on .+")
-        ("INBOX.cleanvm-seosvt"       "^Subject: seosvt.*build on .+")
-        ("INBOX.cleanvm"              "^From: CleanVM bot .*")
-        ("INBOX.cron-daemon"          "^From: .*(Cron Daemon)$")
-        ("INBOX.tasks"                "^From: TeamForge Administrator.*")))
+;;(setq nnimap-split-rule
+;;      '(("INBOX.cleanvm-trustedarm"   "^Subject: trustedarm.*build on .+")
+;;        ("INBOX.cleanvm-apkr10"       "^Subject: apkr10.*build on .+")
+;;        ("INBOX.cleanvm-bereg"        "^Subject: bereg.*build on .+")
+;;        ("INBOX.cleanvm-seosvt"       "^Subject: seosvt.*build on .+")
+;;        ("INBOX.cleanvm"              "^From: CleanVM bot .*")
+;;        ("INBOX.cron-daemon"          "^From: .*(Cron Daemon)$")
+;;        ("INBOX.tasks"                "^From: TeamForge Administrator.*")))
 
 (setq gnus-visual           t
       gnus-large-newsgroup  2000
       gnus-read-active-file 'some)
 
  ;; let Gnus change the "From:" line by looking at current group we are in.
-(setq gnus-posting-styles
-      '(("erlang-questions" (address "zuav@yandex.ru"))
-        ("ejabberd"         (address "zuav@yandex.ru"))
-        ("debian-announce"  (address "zuav@yandex.ru"))
-        ("debian-russian"   (address "zuav@yandex.ru"))))
+;;(setq gnus-posting-styles
+;;      '(("erlang-questions" (address "zuav@yandex.ru"))
+;;        ("ejabberd"         (address "zuav@yandex.ru"))
+;;        ("debian-announce"  (address "zuav@yandex.ru"))
+;;        ("debian-russian"   (address "zuav@yandex.ru"))))
 
 ;;; .gnus.el ends here
